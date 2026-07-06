516bca2d3333e5297cf9b4bf04981f7d294100bf
Tharindi Anuththara <tjayasena64@gmail.com>
2026-07-06 22:19:53 +0530

    Delete HotelERP directory

--- HotelERP/HotelERP/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HotelERP.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
