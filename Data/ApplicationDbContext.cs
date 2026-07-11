bff195e4a102c8dc35d844c5622a48a64fb6d5cd
Delika12345 <delikanayomi2002@gmail.com>
2026-07-11 20:33:03 +0530

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GrandHotel.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // චාමිකගේ කිසිම කෝඩ් එකකට හානියක් නැතිව ඔයාගේ ටේබල් එක විතරක් මෙතන හැදෙනවා
+        public DbSet<Reservation> Reservations { get; set; }
+    }
+}
